bd19bfc475d80d81a1199fa49b544a29def3a489
irina777-create <71390012+irina777-create@users.noreply.github.com>
2023-11-01 20:09:59 +0200

    создание main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+//Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+const array  = [1, 2, 3, 4, 5, 6, 71]
+console.log (array);
+const removeElement = ( array, item)=> {
+    const indexOflItem = array.indexOf (item);
+    if  (index !== -1) {
+        array. splice (index, 1);
+    }
+}
+removeElement (array,item:4);
+consol.log(array);
